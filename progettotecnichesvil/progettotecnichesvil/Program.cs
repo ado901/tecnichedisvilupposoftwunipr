f0f26ed57b704a3d14e2fb3b035493509d4d504e
ado901 <xadozz@gmail.com>
2021-05-02 01:51:23 +0200

    nuova versione con login-entity con azure

---
+++ progettotecnichesvil/progettotecnichesvil/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progettotecnichesvil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool loggato = false;
+            try
+            {
+                var wcf = new ServiceReference1.Service1Client();
+                Console.WriteLine("wcf creato");
+                try
+                {
+                    Console.WriteLine("tentativo di connessione...");
+                    wcf.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Connessione fallita: " + ex.Message);
+                }
+
+                wcf.DoWork();
+                wcf.Dowork1("ciao Andrea");
+                Console.WriteLine("MENU'");
+                bool loop = true;
+                ServiceReference1.Utente ut = new ServiceReference1.Utente();
+                while (loop)
+                {
+                    
+                    switch (loggato)
+                    {
+                        case false:
+                            
+                            Console.WriteLine("1. Registrazione");
+                            Console.WriteLine("2. LOGIN");
+                            
+                            var key = Console.ReadKey();
+
+                            switch (key.KeyChar)
+                            {
+                                //REGISTRAZIONE
+                                case '1':
+                                    
+                                    Console.WriteLine("Inserisci il email:");
+                                    ut.email = Console.ReadLine();
+
+                                    //tentativo di convalida formato email fallito lol
+                                    //if (IsValidEmail(ut.email))
+                                    //{
+                                    //    Console.WriteLine("email non valida");
+                                    //    break;
+                                    //}
+
+                                    Console.WriteLine("Inserisci il password:");
+                                    string inputString = Console.ReadLine();
+
+                                    //criptazione sha256
+                                    byte[] data = Encoding.ASCII.GetBytes(inputString);
+                                    data = new System.Security.Cryptography.SHA256Managed().ComputeHash(data);
+                                    ut.password = Encoding.ASCII.GetString(data);
+
+                                    Console.WriteLine("Inserisci il nome:");
+                                    ut.nome = Console.ReadLine();
+
+                                    Console.WriteLine("Inserisci il cognome:");
+                                    ut.cognome = Console.ReadLine();
+
+                                    Console.WriteLine("Inserisci  dataNascita: dd/mm/aaaa");
+                                    string dataNa = Console.ReadLine();
+                                    var datanascita = Utility.checkData(dataNa);
+                                    ut.nascita = new DateTime(datanascita.Item1, datanascita.Item2, datanascita.Item3);
+
+                                    Console.WriteLine("Inserisci  l indirizzo ");
+                                    ut.indirizzo = Console.ReadLine();
+
+                                    ut.portafoglio = 0;
+                                    var registrazione = wcf.Registrazione(ut);
+                                    if (registrazione.Item1)
+                                    {
+                                        Console.WriteLine("registrazione avvenuta");
+                                        loggato = true;
+                                        ut = registrazione.Item2;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("registrazione fallita");
+                                    }
+                                    break;
+
+
+                                //LOGIN
+                                case '2':
+                                    Console.WriteLine("tentativo di login");
+                                    ServiceReference1.Utente utenteregistrato = new ServiceReference1.Utente();
+                                    Console.WriteLine("inserire email:");
+                                    utenteregistrato.email = Console.ReadLine();
+                                    Console.WriteLine("inserire password:");
+                                    //criptazione sha256
+                                    string inputString1 = Console.ReadLine();
+                                    byte[] data1 = Encoding.ASCII.GetBytes(inputString1);
+                                    data1 = new System.Security.Cryptography.SHA256Managed().ComputeHash(data1);
+                                    utenteregistrato.password = Encoding.ASCII.GetString(data1);
+                                    var login = wcf.Login(utenteregistrato);
+                                    if (login.Item1)
+                                    {
+                                        Console.WriteLine("login effettuato");
+                                        loggato = true;
+                                        ut = login.Item2;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("login fallito");
+                                    }
+                                    break;
+
+                                default:
+                                    loop = false;
+                                    break;
+                            }
+                            break;
+
+
+
+                        case true:
+                            Console.WriteLine("Utente " + ut.nome + " " + ut.cognome+", benvenuto");
+                            Console.ReadLine();
+                            loop = false;
+                            break;
+
+                    }
+                    
+
+                }
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Errore: " + ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+
+        //funzione trovata su stackoverflow per convalidare email
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    
+}
+}
---
+++ progettotecnichesvil/progettotecnichesvil/Utility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progettotecnichesvil
+{
+    class Utility
+    {
+        public static (int,int,int) checkData(string dataNa)
+        {
+            if (dataNa.Substring(2, 1) != "/" || dataNa.Substring(5, 1) != "/")
+            {
+                throw new Exception("Formato della data non valido! inserire una data nel formato dd/mm/aaaa");
+            }
+            int giorni = 0;
+            if (int.TryParse(dataNa.Substring(0, 2), out giorni))
+            {
+                if (giorni < 0 || giorni > 31)
+                {
+                    throw new Exception("Inserire un valore per i giorni compreso tra 1 e 31");
+                }
+            }
+            else
+            {
+                throw new Exception("Giorni errato");
+            }
+            int mese = 0;
+            if (int.TryParse(dataNa.Substring(3, 2), out mese))
+            {
+                if (mese < 0 || mese > 12)
+                {
+                    throw new Exception("Inserire un valore per i mesi compreso tra 1 e 12");
+                }
+            }
+            else
+            {
+                throw new Exception("mese errato");
+            }
+            int anno = 0;
+            if (int.TryParse(dataNa.Substring(6, 4), out anno))
+            {
+
+            }
+            else
+            {
+                throw new Exception("anno errato");
+            }
+
+            return (anno, mese, giorni);
+        }
+
+    }
+}
